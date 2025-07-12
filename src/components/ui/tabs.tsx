255d1972e23514582c5230be83bd86e458fa9768
MikeRubio <miguel.rubiojimenez@gmail.com>
2025-07-12 16:37:16 +0200

    chore: tabs style change

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -27,7 +27,8 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-black data-[state=active]:shadow',
+      "inline-flex mx-1 items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium hover:border-black ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black",
+      "data-[state=active]:bg-black data-[state=active]:text-white data-[state=active]:shadow",
       className
     )}
     {...props}
